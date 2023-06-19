a021e089ef8b08cb0c2bc57e0da4496ef8e42fdc
hyunjin920 <hyunjin920@naver.com>
2023-06-19 23:35:56 +0900

    sssasdasdasd

--- java_project/src/java_project/TestClass.java
+++ java_project/src/java_project/TestClass.java
@@ -3,7 +3,7 @@ package java_project;
 public class TestClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method s tub
 		System.out.println("test");
 	}
 
